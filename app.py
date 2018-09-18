26b71ba5ae259b02b8c62e0caf3e100c2b869533
Austin <austinbrown34@gmail.com>
2018-09-18 18:26:22 -0400

    write to tmp

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, make_response, request, abort
 from ocrlib import OCR
-
+import requests
 
 app = Flask(__name__)
 
@@ -10,9 +10,13 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def ocr_lambda():
-    text = OCR.get_text('images/example_03.png')
+    img = request.args.get('img')
+    r = requests.get(img)
+    with open('/tmp/img.png', 'wb') as f:
+        f.write(r.content)
+    text = OCR.get_text('/tmp/img.png')
     return text
 
 
--- ocrlib.py
+++ ocrlib.py
@@ -3,24 +3,24 @@ import pytesseract
 import cv2
 import os
 
+
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 LIB_DIR = os.path.join(SCRIPT_DIR, 'lib')
 os.environ['LD_LIBRARY_PATH'] = os.environ['LAMBDA_TASK_ROOT'] + LIB_DIR
 os.environ['TESSDATA_PREFIX'] = SCRIPT_DIR
 
 class OCR(object):
-
     @staticmethod
     def get_text(image_path, preprocess=None):
         image = cv2.imread(image_path)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         if preprocess == "thresh":
             gray = cv2.threshold(gray, 0, 255,
-        		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        	   cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         elif preprocess == "blur":
             gray = cv2.medianBlur(gray, 3)
 
-        filename = "{}.png".format(os.getpid())
+        filename = "/tmp/{}.png".format(os.getpid())
         cv2.imwrite(filename, gray)
         text = pytesseract.image_to_string(Image.open(filename))
         os.remove(filename)
