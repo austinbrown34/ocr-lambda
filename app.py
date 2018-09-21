68e7bc20a502772712779691e621a5dbf67a52cb
Austin Brown <browna4@Administrators-iMac.local>
2018-09-21 14:39:28 -0400

    Add convert route and task

--- app.py
+++ app.py
@@ -15,21 +15,28 @@ app = Flask(__name__)
 session = boto3.Session()
 s3 = session.resource('s3')
 bucket = 'ocr-lambda-text'
+converted_bucket = 'ocr-lambda-converted'
 
 
 @task
-def upload_text(text):
+def convert(file):
+    r = requests.get(file)
     try:
-        os.remove('/tmp/text.txt')
+        os.remove('/tmp/file')
     except OSError:
         pass
-    with open('/tmp/text.txt', 'w') as f:
-        f.write(text)
+    with open('/tmp/file', 'wb') as f:
+        f.write(r.content)
+    subprocess.call(['convert', '-verbose', '-density', '150', '-trim', '/tmp/file', '-quality', '100', '-flatten', '-sharpen', '0x1.0', '-append', '/tmp/file.png'])
+    upload('/tmp/file.png', converted_bucket, 'png-{}.png'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')))
 
+
+@task
+def upload(local_file, bucket, s3_file):
     s3.meta.client.upload_file(
-        '/tmp/text.txt',
+        local_file,
         bucket,
-        'text-{}.txt'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'))
+        s3_file
     )
 
 
@@ -43,12 +50,17 @@ def get_text(file):
     with open('/tmp/file', 'wb') as f:
         f.write(r.content)
     if magic.from_file('/tmp/file', mime=True) != 'image/png':
-        # convert -verbose -density 150 -trim Austin\ Brown\ Resume\ 2018\ -\ Full\ Stack\ Engineer.pdf -quality 100 -flatten -sharpen 0x1.0 res.tiff
         subprocess.call(['convert', '-verbose', '-density', '150', '-trim', '/tmp/file', '-quality', '100', '-flatten', '-sharpen', '0x1.0', '-append', '/tmp/file.png'])
     else:
         os.rename('/tmp/file', '/tmp/file.png')
     text = OCR.get_text('/tmp/file.png')
-    upload_text(text)
+    try:
+        os.remove('/tmp/text.txt')
+    except OSError:
+        pass
+    with open('/tmp/text.txt', 'w') as f:
+        f.write(text)
+    upload('/tmp/text.txt', bucket, 'text-{}.txt'.format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')))
 
 
 @app.errorhandler(404)
@@ -56,9 +68,27 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def ocr_lambda():
-    get_text(request.args.get('file'))
+    if request.method == 'GET':
+        file = request.args.get('file')
+    elif request.method == 'POST':
+        if not (request.json):
+            abort(400)
+        file = request.json['file']
+    get_text(file)
+    return "Request Received"
+
+
+@app.route('/convert', methods=['GET', 'POST'])
+def convert():
+    if request.method == 'GET':
+        file = request.args.get('file')
+    elif request.method == 'POST':
+        if not (request.json):
+            abort(400)
+        file = request.json['file']
+    convert(file)
     return "Request Received"
 
 
